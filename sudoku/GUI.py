0866f0465b37c3696f940e0bf3a74767f69d8c2c
zBrucyP <zbrucyp@gmail.com>
2020-01-10 20:03:39 -0500

    +clearButton +eventHandler +GUI functions

--- sudoku/GUI.py
+++ sudoku/GUI.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-
+# helpful for GUI: http://newcoder.io/gui/part-3
+# https://www.youtube.com/watch?v=jE-SpRI3K5g&list=PLDyQo7g0_nsULCDha3lv7rw2t6AnhNTHV&index=4
 
 class Sudoku_GUI:
     def __init__(self, win_HT=800, win_WT=800,bg_color='#FF9D88'):
@@ -20,6 +21,7 @@ class Sudoku_GUI:
         """
         # root object, highest point in GUI, window
         GUI = tk.Tk()
+        GUI.title('Sudoku')
 
         # background to fill in the window
         canvas = tk.Canvas(GUI,
@@ -35,12 +37,24 @@ class Sudoku_GUI:
         toolbar = tk.Frame(GUI, bg='white')
         toolbar.place(relwidth=.8, relheight=.1, relx=.1, rely=.05)
 
+        # buttons to go in the toolbar
+        clear_button = tk.Button(toolbar, text='Clear Answers', command=self.clear_answers)
+        clear_button.pack()
+
         # frame for game board
         game_board = tk.Frame(GUI, bg='white')
         game_board.place(relwidth=.8, relheight=.8, relx=.1, rely=.2)
 
-        # pack elements to their parents
-        #canvas.pack()
-
+        # set event handlers for the game board
+        game_board.bind("<Button-1>", self.cell_clicked)
 
         return GUI
+
+    def cell_clicked(self, event):
+        1+1
+
+    def draw_grid(self):
+        1+1
+
+    def clear_answers(self):
+        1+1
